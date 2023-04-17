b62cd318e1b0904bd367804afb5f14a44439fc50
Aeternus185 <andryp_@hotmail.com>
2023-04-17 07:00:47 -0400

    fix: 🐛 change the themeSwitch icon when theme changes

--- src/scripts/themeSwitch.js
+++ src/scripts/themeSwitch.js
@@ -1,4 +1,6 @@
 const themeSwitchButton = document.getElementById("themeSwitch");
+const sunIcon = document.querySelector(".fa-sun");
+const moonIcon = document.querySelector(".fa-moon");
 
 const themeCheck = () => {
   const userTheme = localStorage.getItem("theme");
@@ -6,19 +8,27 @@ const themeCheck = () => {
 
   if (userTheme === "dark" || (!userTheme && systemTheme)) {
     document.documentElement.classList.add("dark");
+    sunIcon.classList.remove("hidden");
+    moonIcon.classList.add("hidden");
     themeSwitchButton.checked = false;
     return;
   }
+  sunIcon.classList.add("hidden");
+  moonIcon.classList.remove("hidden");
 };
 
 const themeSwitch = () => {
   if (document.documentElement.classList.contains("dark")) {
     document.documentElement.classList.remove("dark");
+    sunIcon.classList.add("hidden");
+    moonIcon.classList.remove("hidden");
     themeSwitchButton.checked = true;
     localStorage.setItem("theme", "light");
     return;
   }
   document.documentElement.classList.add("dark");
+  sunIcon.classList.remove("hidden");
+  moonIcon.classList.add("hidden");
   themeSwitchButton.checked = false;
   localStorage.setItem("theme", "dark");
 };
