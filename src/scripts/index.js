6c6ae0eb1bce85afecdc458c8317575c0541fef8
Aeternus185 <andryp_@hotmail.com>
2023-04-17 00:47:15 -0400

    chore: 🔥 remove unnecessary code from boilerplate
    remove counter and translations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,6 @@
 import "../styles/style.css";
 import { initThemeSwitch } from "./themeSwitch.js";
 import { initLangSwitch } from "./langSwitch.js";
-import { initCounter } from "./counter.js";
 
 initThemeSwitch();
 initLangSwitch();
-initCounter("#counter");
--- src/scripts/utils/translations.js
+++ src/scripts/utils/translations.js
@@ -1,9 +1,8 @@
 import i18next from "i18next";
-import { counter } from "../counter";
 
 export function updateTranslations() {
   document.querySelectorAll("[data-i18n]").forEach((element) => {
     const key = element.getAttribute("data-i18n");
-    element.textContent = i18next.t(key, { count: counter });
+    element.textContent = i18next.t(key);
   });
 }
