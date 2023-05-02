b4d91e65313a0b61b5e56b5d908f5153fec1ef79
Aeternus185 <andryp_@hotmail.com>
2023-05-02 12:52:26 -0400

    feat: ✨ add openAi util for fetch

---
+++ src/scripts/utils/openAi.js
@@ -0,0 +1,22 @@
+const params = {
+  model: "text-davinci-002",
+  prompt: "",
+};
+
+async function openAi(prompt) {
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI_SECRET_KEY}`,
+    },
+    body: JSON.stringify({
+      ...params,
+      prompt,
+    }),
+  });
+  const data = await response.json();
+  return data.choices[0].text.trim();
+}
+
+export { openAi };
