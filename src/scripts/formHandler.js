fc40c20ae4769807f1902d8de2b53c0d2b435eaf
Aeternus185 <andryp_@hotmail.com>
2023-05-05 17:59:20 -0400

    feat: ✨ add call getGiftIdeas and export giftData

--- src/scripts/formHandler.js
+++ src/scripts/formHandler.js
@@ -1,18 +1,21 @@
 import { budgetRange } from "./budget.js";
 import { interests } from "./interests.js";
+import { getGiftIdeas } from "./giftIdeas.js";
+
+const giftData = {};
 
 const form = document.querySelector("#giftForm");
 
 function formHandler(e) {
   e.preventDefault();
   const giftFormData = new FormData(e.target);
-  const giftData = {};
   giftFormData.forEach((value, key) => (giftData[key] = value));
   giftData.interests = interests.toString();
   giftData.budget = budgetRange();
-  console.log(giftData);
+
+  getGiftIdeas();
 }
 
 form.addEventListener("submit", formHandler);
 
-export { formHandler };
+export { formHandler, giftData };
