a0390fb6c8a0d511dba05e851a92bc844a629d1c
Aeternus185 <andryp_@hotmail.com>
2023-05-06 13:50:22 -0400

    fix: 🐛 fix classes for center all

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -2,7 +2,7 @@ function showLoader() {
   const main = document.querySelector("main");
   main.innerHTML = "";
   main.className =
-    "flex flex-col gap-4 h-full items-center justify-center px-4 py-4 lg:px-8";
+    "flex h-full flex-col items-center justify-center gap-4 px-4 py-4 text-center lg:px-8";
   main.innerHTML = `<h1 class="text-2xl font-bold">Giftstorming in Progress✨</h1><lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_uknnyrjt.json"  background="transparent"  speed="1"  style="width: 96px; height: 96px;"  loop  autoplay></lottie-player>`;
 }
 
